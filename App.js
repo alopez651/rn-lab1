e29e46705003096b93271f1efbe10dd0e3ac2128
Ana Lopez <analopez@Anas-MacBook-Pro.local>
2021-07-08 14:38:01 -0700

    adding rn

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import * as React from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.photoContainer}>
-        <Image source={require("./assets/perry.png")} style={styles.photo} />
+        <Image source={require("./assets/bit.jpg")} style={styles.photo} />
       </View>
       <View style={styles.nameContainer}>
-        <Text style={styles.nameAndPronouns}>Perry (she/her)</Text>
+        <Text style={styles.nameAndPronouns}>Ana Lopez (She/Her)</Text>
       </View>
       <View style={styles.blurbContainer}>
-        <Text style={styles.blurb}>chirp chirp i'm a parakeet</Text>
+      <Text style={styles.blurb}>My Info </Text>
+        <TouchableOpacity onPress = {() => alert("linkedin.com/in/ana-lopez-38573a20a                                github.com/alopez651")}
+         style = {styles.button}>
+           <Text style={styles.buttonText}>Click</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -25,28 +29,47 @@ const styles = StyleSheet.create({
     flex: 3,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "yellow",
-    padding: 30,
+    backgroundColor: "black",
+    padding: 80,
   },
   nameContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "azure",
+    fontFamily: "Courier New",
   },
   blurbContainer: {
     flex: 2,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "azure",
+    fontFamily: "Courier New",
   },
   photo: {
     resizeMode: "contain",
     width: "90%",
-    padding: 50,
+    padding: 100,
   },
   nameAndPronouns: {
-    fontSize: 50,
+    color: 'black',
+    fontSize: 30,
+    fontFamily: "Courier New",
   },
   blurb: {
     fontSize: 20,
+    fontFamily: "Courier New",
+  },
+  button: {
+    backgroundColor: "yellow",
+    padding: 20,
+    borderRadius: 5,
+    backgroundColor: "white"
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "black",
+    backgroundColor: "azure",
+    fontFamily: "Courier New",
   },
 });
